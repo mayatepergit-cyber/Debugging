82da05066dd062becef8c2054225d621f2ce04df
mayatepergit-cyber <mayatepergit@gmail.com>
2025-12-01 19:31:01 +0200

    Fixed Shapes

--- DebugWorkshop/Shapes/triangle.cpp
+++ DebugWorkshop/Shapes/triangle.cpp
@@ -6,12 +6,8 @@ Triangle::Triangle(float base, float height) : _base(base), _height(height)
 {
 }
 
-float Triangle::get_area(bool has_depth) const
-{
-	if (has_depth)
-	{
-		throw std::exception("3D triangle is not implemented yet!");
-	}
+float Triangle::get_area() const // removed the parameter
 
+{
 	return float(0.5 * _base * _height);
 }
